0863305a0f93448d550080f7c0493a6c53b4f397
多幸运 <389572613@qq.com>
2021-04-19 23:44:09 +0800

    0419

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -99,7 +99,7 @@ class Index extends Controller
             }
               $list=db('admin_example')->alias('a')->join('admin_example_menu b','a.menu_id=b.id')->where('b.menu_type',1)->where($map)->field('a.*')->paginate(8);
                 $this->assign("list",$list);
-                var_dump($list);
+//                var_dump($list);
 
         }
             $menu_list=db('admin_example_menu')->where('menu_type',1)->select();
